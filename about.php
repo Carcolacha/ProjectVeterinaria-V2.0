bfb0f2d397a9dbe42def68dc49adaaa56ead8ed3
Edwyn Urbina <82457708+Carcolacha@users.noreply.github.com>
2021-11-30 10:32:01 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php include("template/header.php"); ?>
+
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Creamos sitio web epico</p>
+                <hr class="my-2">
+            </div>
+
+<?php include("template/footer.php"); ?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include("template/header.php"); ?>
+
+    <div class="col-md-2">
+        
+    </div>
+            <div class="col-md-8">
+                <div class="card text-left">
+                  <img class="card-img-top" src="holder.js/100px180/" alt="">
+                  <div class="card-body">
+                    <div class="content">
+                        <div class="info-card">
+                        <h4 class="card-title">Contamos con Servicio 24/7</h4>
+                        <p class="card-text">Contamos con profesionales perparados para atencion cualquier situación. las 24 horas del día, los 7 días de la semana.</p>
+                        </div>
+                        <div class="col-md-6">
+                            <img src="img/CatHappy.png" alt="CatHappy" class="CatHappy">
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            
+<?php include("template/footer.php"); ?>
